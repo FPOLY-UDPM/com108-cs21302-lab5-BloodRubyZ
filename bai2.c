a4d62b909365cea9fa9757bd02898c196d8f3346
BloodRubyZ <khoandaps39836@gmail.com>
2026-02-28 11:09:07 +0700

    AnhKhoa_PS39836

--- bai2.c
+++ bai2.c
@@ -12,12 +12,39 @@
  
 #include <stdio.h>
 
-//Tạo hàm 
+// Tạo hàm kiểm tra năm nhuận bằng cách tách nhỏ các lệnh if
+int kiemTraNamNhuan(int nam) {
+    // Trường hợp 1: Nếu chia hết cho 400 -> Chắc chắn là năm nhuận
+    if (nam % 400 == 0) {
+        return 1; 
+    }
+    
+    // Trường hợp 2: Nếu chia hết cho 4 NHƯNG lại chia hết cho 100 -> KHÔNG phải năm nhuận
+    if (nam % 4 == 0) {
+        if (nam % 100 == 0) {
+            return 0; 
+        } else {
+            return 1; // Chia hết cho 4 và không chia hết cho 100 -> Là năm nhuận
+        }
+    }
+
+    // Trường hợp 3: Không lọt vào các điều kiện trên (không chia hết cho 4)
+    return 0; 
+}
 
 int main() {
+    int nam;
 
-    // //Gọi hàm trong hàm main 
+    printf("Nhập vào một năm bất kỳ: ");
+    scanf("%d", &nam);
+
+    int ketQua = kiemTraNamNhuan(nam);
+    
+    if (ketQua == 1) {
+        printf("%d là năm nhuận.\n", nam);
+    } else {
+        printf("%d không phải là năm nhuận.\n", nam);
+    }
     
     return 0;
 }
-
